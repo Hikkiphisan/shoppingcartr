1e2696dbf828c8734bf89e4efd3637d7d9f3da24
Hikkiphisan <locphihuu@gmail.com>
2025-02-05 16:13:48 +0700

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ProductsList from './components/ProductsList'
+import Cart from './components/Cart'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+      <div>
+        <h2>Shopping Cart</h2>
+        <hr/>
+        <ProductsList />
+        <hr/>
+        <Cart />
+      </div>
+  )}
+
+export default App
--- src/api/shop.js
+++ src/api/shop.js
@@ -1,9 +1,11 @@
-export default {
-    // Hàm lấy danh sách sản phẩm, gọi callback cb sau 1 khoảng thời gian
-    getProducts: (cb, timeout) =>
-        setTimeout(() => cb(_products), timeout || TIMEOUT),
+/**
+ * Mocking client-server processing
+ */
+import _products from './products.json'
 
-    // Hàm mô phỏng việc mua sản phẩm, chỉ gọi callback cb sau một khoảng thời gian
-    buyProducts: (payload, cb, timeout) =>
-        setTimeout(() => cb(), timeout || TIMEOUT)
+const TIMEOUT = 100
+
+export default {
+    getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+    buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
